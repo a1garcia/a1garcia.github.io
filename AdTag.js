c942c36833195990c59b2153d29f971968f88dec
Alfredo Garcia <alfredo_garcia@apple.com>
2017-09-08 11:39:52 -0700

    update

--- AdTag.js
+++ AdTag.js
@@ -188,7 +188,7 @@ hAT.Hackathon = class {
 
     handleMainAction (event) {
         // This can be safely called since we're using the hAT.MRAIDShim
-        mraid.open("https://simpl.info/bigimage/");
+        mraid.open("google.com");
         // window.navigate("https://simpl.info/bigimage/");
     }
 };
